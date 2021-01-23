ef0329683983c440863cb3a3101d2161437c7900
Palloi Hofmann <palloi.hofmann@gmail.com>
2021-01-23 11:54:56 -0300

    message to delete

--- lib/ex_mon_api_web/controllers/fallback_controller.ex
+++ lib/ex_mon_api_web/controllers/fallback_controller.ex
@@ -1,10 +1,10 @@
 defmodule ExMonApiWeb.FallbackController do
   use ExMonApiWeb, :controller
 
-  def call(conn, {:error, changeset}) do
+  def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
     |> put_view(ExMonApiWeb.ErrorView)
-    |> render("400.json", result: changeset)
+    |> render("400.json", result: result)
   end
 end
