f71290d582be8ffd48a571c673b394fc5ccebd25
Palloi Hofmann <palloi.hofmann@gmail.com>
2021-01-23 11:46:41 -0300

    trainer delete

--- lib/ex_mon_api.ex
+++ lib/ex_mon_api.ex
@@ -2,4 +2,5 @@ defmodule ExMonApi do
   alias ExMonApi.Trainer
 
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
 end
--- lib/ex_mon_api_web/controllers/trainers_controller.ex
+++ lib/ex_mon_api_web/controllers/trainers_controller.ex
@@ -9,6 +9,20 @@ defmodule ExMonApiWeb.TrainersController do
     |> handle_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMonApi.delete_trainer()
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete({:error, _reason} = error, _conn), do: error
+
   defp handle_response({:ok, trainer}, conn) do
     conn
     |> put_status(:created)
